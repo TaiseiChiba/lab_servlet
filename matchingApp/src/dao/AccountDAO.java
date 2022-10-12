af47c2577faed94171de2689510b6f5533ad516f
千葉　泰誠 <chiba_taisei@plise.co.jp>
2022-10-13 00:10:56 +0900

    db接続

---
+++ matchingApp/src/dao/AccountDAO.java
@@ -0,0 +1,49 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class AccountDAO {
+	private Connection db;
+	private PreparedStatement ps;
+	private ResultSet rs;
+
+	private void connect() throws NamingException, SQLException {
+		Context context = new InitialContext();
+		DataSource ds = (DataSource) context.lookup("java:comp/env/matchingApp");
+		this.db = ds.getConnection();
+	}
+
+	private void disconnect()  {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (ps != null) {
+				ps.close();
+			}
+			if (db != null) {
+				db.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public void ConnectCheck() {
+		try {
+			this.connect();
+			System.out.println("OK");
+		} catch (NamingException | SQLException e) {
+			e.printStackTrace();
+		}finally {
+			this.disconnect();
+		}
+	}
+}
